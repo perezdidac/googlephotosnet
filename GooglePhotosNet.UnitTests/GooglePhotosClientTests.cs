200a7db8a1d8f9e1ccf4f652b93a3021363332a6
Didac Perez Parera <diperezp@microsoft.com>
2018-09-14 15:41:04 -0700

    Initializing the repo

---
+++ GooglePhotosNet.UnitTests/GooglePhotosClientTests.cs
@@ -0,0 +1,16 @@
+// <copyright file="GooglePhotosClientTests.cs" company="The Bald Labs">
+// Copyright (c) The Bald Labs. All rights reserved.
+// </copyright>
+
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace GooglePhotosNet.UnitTests
+{
+    public class GooglePhotosClientTests
+    {
+    }
+}
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+#pragma warning restore SA1600 // Elements should be documented
---
+++ GooglePhotosNet/GooglePhotosClient.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="GooglePhotosClient.cs" company="The Bald Labs">
+// Copyright (c) The Bald Labs. All rights reserved.
+// </copyright>
+
+namespace GooglePhotosNet
+{
+    /// <summary>
+    /// Client class to interact with the Google Photos Library API.
+    /// </summary>
+    public class GooglePhotosClient
+    {
+    }
+}
